137d548c4ca804f8df5df07a2f85d2ecb1686b26
BachuShravani <shravani17695@gmail.com>
2024-05-17 10:37:54 +0100

    added dburl env variable

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const userRoutes = require('./routes/users');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 // 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl);
 
